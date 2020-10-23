d7ba4ad16f0487d5d28a076fc9763a01f37c715c
will <wye139629@gmail.com>
2020-10-23 22:11:41 +0800

    render 9x9 by using for loop

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      multiplier : [1,2,3,4,5,6,7,8,9],
+      multiplicand : [1,2,3,4,5,6,7,8,9]
+    }
+  }
+  render() {
+    const {multiplier, multiplicand} = this.state
+    let numberItems = []
+    let item
+    for(let i = 0; i < multiplier.length; i++){
+        item = <NumberItem multiplier = {multiplier[i]} multiplicand = {multiplicand}/>
+        numberItems.push(item)
+    }
+    return (
+      <div>
+        {numberItems}
+      </div>
+    )
+  }
+}
+class NumberItem extends Component{
+  render(){
+    const multiplier = this.props.multiplier
+    const multiplicand = this.props.multiplicand
+    let multiplyItmes = []
+    let multiplyItme
+    for(let i = 0; i < multiplicand.length; i++){
+      multiplyItme = <Multiply multiplier = {multiplier} multiplicand = {multiplicand[i]}/>
+      multiplyItmes.push(multiplyItme)
+    }
+    return(
+     <div>
+       <h2>
+        {multiplier}
+       </h2>
+       <ul>
+          {multiplyItmes}
+       </ul>
+     </div>
+    )
+  }
 }
 
-export default App;
+class Multiply extends Component{
+
+  render(){
+    const multiplier = this.props.multiplier
+    const multiplicand = this.props.multiplicand
+    return(
+      <li>{multiplier + "*" +multiplicand + "=" + multiplier * multiplicand}</li>
+    )
+  }
+}
